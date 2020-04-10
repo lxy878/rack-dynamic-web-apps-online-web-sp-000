920135dc9467edba089fb7fce6ca1c24bdad760d
Xiaoyi Liu <lxy878@gmail.com>
2020-04-10 03:52:04 +0000

    Done.

--- app/application.rb
+++ app/application.rb
@@ -2,7 +2,17 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
-    resp.write "Hello, World"
+
+    n1 = Kernel.rand(1..20)
+    n2 = Kernel.rand(1..20)
+    n3 = Kernel.rand(1..20)
+
+    if n1 == n2 and n2 == n3
+      resp.write "You Win"
+    else
+      resp.write "You Lose"
+    end
+
     resp.finish
   end
 

26c6010678a7f117cdf54efcfa37d5f529aa098a
Xiaoyi Liu <lxy878@gmail.com>
2020-04-10 03:50:17 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -2,6 +2,12 @@ class Application
 
   def call(env)
     resp = Rack::Response.new
+    
+    n1 = Kernel.rand(1..20)
+    n2 = Kernel.rand(1..20)
+    n3 = Kernel.rand(1..20)
+
+    resp = Rack::Response.new
     resp.write "Hello, World"
     resp.finish
   end
